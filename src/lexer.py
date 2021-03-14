b2dc4bb59ca74560b6ba8d0df95cb8bde1d8c1d7
Eyepan <iamteoggy@gmail.com>
2021-03-14 17:04:11 +0530

    Separated Tokens to a file

--- src/lexer.py
+++ src/lexer.py
@@ -1,31 +1,6 @@
 # Importing all errors
 from errors import *
-
-
-# CONSTANTS
-DIGITS = '1234567890'
-
-# Tokens
-TT_INT		= 'INT'
-TT_FLOAT    = 'FLOAT'
-TT_PLUS     = 'PLUS'
-TT_MINUS    = 'MINUS'
-TT_MUL      = 'MUL'
-TT_DIV      = 'DIV'
-TT_LPAREN   = 'LPAREN'
-TT_RPAREN   = 'RPAREN'
-
-# Token Class
-class Token:
-	def __init__(self, type, data = None):
-		self._type = type
-		self._data = data
-
-	def __repr__(self):
-		if self._data: 
-			return f'{self._type}:{self._data}'
-		return f'{self._type}'
-
+from tokens import *
 # Lexer class 
 
 class Lexer:
